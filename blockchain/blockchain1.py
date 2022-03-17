81fd1acd179e2a2922ed364fd00cf88f7dea54f4
tse155 <cfmolinade@gmail.com>
2022-03-17 12:14:24 -0500

    Update blockchain1.py

--- blockchain/blockchain1.py
+++ blockchain/blockchain1.py
@@ -19,8 +19,15 @@ import hashlib
 #se importa Json: JavaScript object notation
 import json
 #vamos a importar el constructor Flask, que permite crear un objeto flask
-from flask import Flask
+from flask import Flask, jsonify 
 
 #Parte 1: crear una cadena de bloques
+#orientada a objetos
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain=[]
+        self.create_block(proof=1, previous_hash= "0")
 
 #Parte 2: minar un bloque de la cadena
