fca3436729754f079fee8d2152a0b690e1dcfb8f
tse155 <cfmolinade@gmail.com>
2022-03-17 11:30:29 -0500

    Update blockchain1.py

--- blockchain/blockchain1.py
+++ blockchain/blockchain1.py
@@ -9,3 +9,18 @@ Created on Thu Mar 17 10:32:19 2022
 #Para instalar:
 #Flask==0.12.2: pip install Flask==1.1.2
 #Cliente HTTP Postman: https://www.getpostman.com/
+
+#Importar librerías
+
+#cada bloque de la cadena debe tener su propio timestamp
+import datetime
+#hashlib:libreria para generar los hashes de las cadenas de bloques
+import hashlib
+#se importa Json: JavaScript object notation
+import json
+#vamos a importar el constructor Flask, que permite crear un objeto flask
+from flask import Flask
+
+#Parte 1: crear una cadena de bloques
+
+#Parte 2: minar un bloque de la cadena
