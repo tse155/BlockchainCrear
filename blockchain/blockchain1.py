a58154780f01fb3e76682dcfd280f42f84bc8fe2
tse155 <cfmolinade@gmail.com>
2022-03-17 10:49:09 -0500

    Update blockchain1.py

--- blockchain/blockchain1.py
+++ blockchain/blockchain1.py
@@ -5,3 +5,7 @@ Created on Thu Mar 17 10:32:19 2022
 @author: the_c
 """
 
+#Crear una cadena de bloques
+#Para instalar:
+#Flask==0.12.2: pip install Flask==0.12.2
+#Cliente HTTP Postman: https://www.getpostman.com/
